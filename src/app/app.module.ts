2d9f4a7b7e72571992c64aee4fd4f1e4a4b8c008
ciceribeiroo <alicinha2r@gmail.com>
2021-04-06 13:42:39 -0300

    Pegar cidade mais proxima da localizacao atual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -16,14 +17,20 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { Storage } from '@ionic/storage-angular';
 import { SearchHistoryService } from 'src/domain/services/search-history-service';
 import { LocalHistoryRepository } from 'src/data/local-history-repository';
-import { CityRepository } from 'src/domain/services/protocols/city-repository';
+import { LocationService } from 'src/domain/services/location-services';
+import { LocalLocationRepository } from 'src/data/local-location-repository';
 
 const createSearchCityService = () => {
   return new SearchCityService(new LocalCityRepository());
 };
+
 const createHistoryService = () => {
   return new SearchHistoryService(new LocalHistoryRepository(new Storage(), new LocalCityRepository()));
-}
+};
+
+const createLocationService = () => {
+  return new LocationService(new LocalLocationRepository( new Geolocation(), new LocalCityRepository()), new Geolocation());
+};
 
 const createLoadWeatherService = (http: HttpClient) => {
   return new LoadWeatherService(
@@ -43,6 +50,11 @@ const createLoadWeatherService = (http: HttpClient) => {
     HttpClientModule,
   ],
   providers: [
+    Geolocation,
+    {
+      provide: LocationService,
+      useFactory: createLocationService,
+    },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     {
       provide: SearchCityService,
